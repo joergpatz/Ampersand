e9eebdd3658758cb91b8bf83eecb9026f19e5223
Thomas Klokosch <mail@thomasklokosch.com>
2014-06-01 01:11:24 +0200

    Added stub for phpunit tests

---
+++ tests/RendererTest.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use \Ampersand\Renderer;
+
+class RendererTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testIfRendererCanBeInitialized()
+    {
+        $renderer = new Renderer();
+        $this->assertTrue( $renderer instanceof Renderer );
+    }
+
+}
+
