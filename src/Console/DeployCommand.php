49cbff4f94d456e0891fdd864d657d66e35f9991
Joerg <joerg.patzer@me.com>
2014-06-05 21:53:44 +0200

    extended DeployCommand for using Logger

--- src/Console/DeployCommand.php
+++ src/Console/DeployCommand.php
@@ -2,9 +2,7 @@
 namespace Ampersand\Console;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
 
@@ -13,48 +11,60 @@ use Monolog\Handler\StreamHandler;
 
 use Ampersand\Deploy\D;
 
-#
-# Deploy Command
-#
+/**
+ * Class for the Deploy Command.
+ *
+ * @package Ampersand\Console
+ */
 class DeployCommand extends Command
 {
 
-    private $log;
+    /**
+     * Monolog Logger instance
+     *
+     * @var \Monolog\Logger
+     */
+    protected $log;
 
 
-    /*
-     * Configure
+    /**
+     * Command Configure
+     *
+     * @return void
      */
     protected function configure()
     {
         $this->setName('deploy')->setDescription('Deploy site');
-        $this->log = new Logger('Deployment');
+        $this->log = new Logger('Deployer');
     }
 
 
-    /*
-     * Execute
+    /**
+     * Execute on Command
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->log = new Logger('Renderer');
         $this->log->pushHandler(new StreamHandler(fopen('php://stdout', 'w')));
 
         $config = Yaml::parse('config/environments.yml');
 
-        $deploy = D::setup($config['testftp']);
+        D::setup($config['testftp'], $this->log);
 
         $this->log->addInfo("Testing connection...");
-        if(D::testConnection('ftp')){
-            $this->log->addInfo("Connection works");
+        if (D::testConnection('ftp')) {
+            $this->log->addInfo("Connection OK.");
         } else {
-            $this->log->addError("Couldn't establish connection");
+            $this->log->addError("Could not establish connection.");
         }
 
-        $this->log->addInfo("Deploying build directory");
+        $this->log->addInfo("Deploying build directory...");
 
         D::syncContents('ftp');
 
-        $this->log->addInfo("Deploying site");
+        $this->log->addInfo("Fine. Site deployed.");
     }
 }
