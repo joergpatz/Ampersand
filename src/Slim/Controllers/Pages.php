c7613e82d0564000b41cd0fb4074cbcf825ee478
Joerg <joerg.patzer@me.com>
2014-06-25 16:33:02 +0200

    setting page properties created_at and updated_at in controller actions

--- src/Slim/Controllers/Pages.php
+++ src/Slim/Controllers/Pages.php
@@ -34,6 +34,8 @@ class Pages
         $page = R::dispense(self::$table);
         // import all values from the body data with a property selection
         $page->import($body, 'title,layout');
+        $page->created_at = R::isoDateTime();
+        $page->updated_at = R::isoDateTime();
 
         R::store($page);
 
@@ -53,6 +55,7 @@ class Pages
 
         // import all values from the body data with a property selection
         $page->import($body, 'title,layout');
+        $page->updated_at = R::isoDateTime();
 
         R::store($page);
 
